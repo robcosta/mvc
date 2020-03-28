0adca88d93f5e1240b7decc2b2c729553602ffd1
Roberto Costa <robertocosta66@gmail.com>
2020-03-28 15:14:35 -0300

    Add function del on UsuariosController.php

--- src/controllers/UsuariosController.php
+++ src/controllers/UsuariosController.php
@@ -2,62 +2,70 @@
 namespace src\controllers;
 
 use \core\Controller;
-use\src\models\Usuario;
+use \src\models\Usuario;
 
-class UsuariosController extends Controller {
+class UsuariosController extends Controller
+{
 
-   public function add(){
-       $this->render('add');
-   }
+    public function add()
+    {
+        $this->render('add');
+    }
 
-   public function addAction(){
-        $nome = filter_input(INPUT_POST,'nome', FILTER_SANITIZE_SPECIAL_CHARS);
-        $email = filter_input(INPUT_POST,'email', FILTER_VALIDATE_EMAIL);
+    public function addAction()
+    {
+        $nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_SPECIAL_CHARS);
+        $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
 
-        if($nome && $email){
+        if ($nome && $email) {
             $data = Usuario::select()->where('email', $email)->execute();
 
-            if(count($data) === 0){
+            if (count($data) === 0) {
                 //insere
                 Usuario::insert([
                     'nome' => $nome,
-                    'email' => $email
+                    'email' => $email,
                 ])->execute();
                 $this->redirect('/');
-            } 
+            }
         }
         $this->redirect('/novo');
-   }
+    }
 
-   public function edit($args){
-       //Proucrar um ítem pelo id use find()
+    public function edit($args)
+    {
+        //Proucrar um ítem pelo id use find()
         $usuario = Usuario::select()->find($args['id']);
 
         $this->render('edit', [
-            'usuario' => $usuario
+            'usuario' => $usuario,
         ]);
     }
 
-    public function editAction($args){
-        $nome = filter_input(INPUT_POST,'nome', FILTER_SANITIZE_SPECIAL_CHARS);
-        $email = filter_input(INPUT_POST,'email', FILTER_VALIDATE_EMAIL);
+    public function editAction($args)
+    {
+        $nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_SPECIAL_CHARS);
+        $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
 
-        if($nome && $email){
-            Usuario::update()
-                ->set('nome', $nome)
-                ->set('email', $email)
-                ->where('id', $args['id'])
-            ->execute();
+        if ($nome && $email) {
+            $data = Usuario::select()->where('email', $email)->execute();
+            $data = $data[0];
+            if (count($data) === 0 || $data['id'] === $args['id']) {
+                Usuario::update()
+                    ->set('nome', $nome)
+                    ->set('email', $email)
+                    ->where('id', $args['id'])
+                    ->execute();
 
-            $this->redirect('/');
+                $this->redirect('/');
+            }
         }
-
-        $this->redirect('/usuario'.$args['id'].'/editar');
-
+        $this->redirect('/usuario/'.$args['id'].'/editar');
     }
-    
-    public function del($args){
-        echo "Deletando: ".$args['id'];
 
-   }
+    public function del($args)
+    {
+        Usuario::delete()->where('id',$args['id'])->execute();
+        $this->redirect("/");
+    }
 }
